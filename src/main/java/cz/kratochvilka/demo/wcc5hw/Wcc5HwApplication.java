d8bd1db2fe8fc186fda5e2418a335e4a25fc8936
pysdenis <d.pys.st@spseiostrava.cz>
2024-03-31 00:58:44 +0100

    init

---
+++ src/main/java/cz/kratochvilka/demo/wcc5hw/Wcc5HwApplication.java
@@ -0,0 +1,13 @@
+package cz.kratochvilka.demo.wcc5hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wcc5HwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wcc5HwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kratochvilka/demo/wcc5hw/Wcc5HwApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kratochvilka.demo.wcc5hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wcc5HwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
